a9f3290407a7dd5393c2f3accc8c4908ad273f32
laurencon <101435564+laurencon@users.noreply.github.com>
2023-11-12 00:51:57 -0800

    Update serverChat.py

--- serverChat.py
+++ serverChat.py
@@ -15,10 +15,12 @@ def broadcast(message):
             
             
 # Function to handle clients connections            
-def handle_client(client):
+def handle_client(client, cursor, connection):
     while True:
         try:
-            message = client.recv(1024)
+            message = client.recv(1024).decode('utf-8')
+            # saving messages to database
+            save_messages(cursor, message, sender_alias)
             broadcast(message)
         except:
             index = clients.index(client)
@@ -28,7 +30,14 @@ def handle_client(client):
             broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
             aliases.remove(alias)
             break
-        
+# Function to insert the message into database
+def save_messages(cursor, message, sender_alias):
+    sender = sender_alias
+    # we need to split the message itself to extract
+    content = message.split(':', 1)[1].strip()
+    cursor.execute("INSERT INTO messages (sender, receiver, content) VALUES (%s, %s, %s)",
+                   (sender, 'Placeholder', content))
+    connection.commit()
 # Main function to receive the connection
 def receive():
     while True:
@@ -41,9 +50,18 @@ def receive():
         clients.append(client)
         print(f'The alias of this client is {alias}'.encode('utf-8'))
         broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+
+        #send chat history
+        send_chat_history(client, cursor)
         client.send('You are now connected!'.encode('utf-8'))
         thread = threading.Thread(target = handle_client, args=(client,))
         thread.start()
+
+def send_chat_history(client, cursor):
+    cursor.execute("SELECT * FROM messages ORDER BY timestamp")
+    messages = cursor.fetchall()
+    for message in messages:
+        client.send(str(message).encode('utf-8'))
 
 if __name__ == '__main__':
     receive()
