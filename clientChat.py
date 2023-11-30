c6de0856a91414704b1dafedba2059fba0f7c56c
ericaatann <ericatan0302@yahoo.com>
2023-11-30 15:19:17 -0800

    updated

--- clientChat.py
+++ clientChat.py
@@ -1,35 +1,40 @@
 import socket
 import threading
 
-alias = input('Choose an alias >>> ')
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59000))
-
-def client_receive(): 
+def client_receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            if message == "alias?":
-                client.send(alias.encode('utf-8'))
-            else:
-                print(message)
+            print(message)
         except Exception as e:
             print(f'Error: {e}')
-            client.close()
             break
 
 def client_send():
     while True:
         try:
-            message = input(f'{alias}: ')
+            message = input()
             if message.lower() == "<exit>":
                 client.send(message.encode('utf-8'))
                 break
-            client.send(f'{alias}: {message}'.encode('utf-8'))
+            client.send(message.encode('utf-8'))
         except Exception as e:
             print(f'Error: {e}')
-            client.close()
             break
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 59000))
+
+while True:
+    alias = input('Enter your alias: ')
+    client.send(alias.encode('utf-8'))
+    response = client.recv(1024).decode('utf-8')
+    
+    if response == alias:
+        print(f"Alias '{alias}' is already in use. Please choose another one.")
+    else:
+        print(response)
+        break
 
 receive_thread = threading.Thread(target=client_receive)
 receive_thread.start()
