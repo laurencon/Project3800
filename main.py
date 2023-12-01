71b50b206b523a85824ccc7a6ad488467fabb961
ericaatann <ericatan0302@yahoo.com>
2023-11-30 16:14:36 -0800

    added code for duplicate names

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template,request,session,redirect,url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "sectretKey"
+app.config["SECRET_KEY"] = "secretKey"
 
 socketio = SocketIO(app)
 
 rooms = {}
+
 
 def generate_unique_code(length):
     while True:
@@ -17,7 +19,15 @@ def generate_unique_code(length):
         if code not in rooms:
             break
     return code
-@app.route("/", methods=["POST","GET"])
+
+
+def is_name_available(room, name):
+    if room in rooms and "used_names" in rooms[room]:
+        return name not in rooms[room]["used_names"]
+    return True
+
+
+@app.route("/", methods=["POST", "GET"])
 def home():
     session.clear()
     if request.method == "POST":
@@ -29,18 +39,22 @@ def home():
         if not name:
             return render_template("home.html", error="Please enter a name.", code=code, name=name)
 
+        if not is_name_available(code, name):
+            return render_template("home.html", error="Name already exists in the room. Please choose another name.", code=code, name=name)
+
         if join != False and not code:
-            return render_template("home.html", error="Please enter a room code.",code=code, name=name)
+            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
 
         room = code
         if create != False:
             room = generate_unique_code(4)
-            rooms[room] = {"members": 0, "messages" : []}
+            rooms[room] = {"members": 0, "messages": [], "used_names": set()}
         elif code not in rooms:
             return render_template("home.html", error="Room does not exist.", code=code, name=name)
 
         session["room"] = room
         session["name"] = name
+        rooms[room]["used_names"].add(name)
         return redirect(url_for("room"))
 
     return render_template("home.html")
@@ -64,6 +78,7 @@ def message(data):
     send(content, to=room)
     rooms[room]["messages"].append(content)
     print(f"{session.get('name')} said: {data['data']}")
+    
 @socketio.on("connect")
 def connect(auth):
     room = session.get("room")
@@ -92,4 +107,4 @@ def disconnect():
     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app,debug=True)
+    socketio.run(app, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
