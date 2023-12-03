683eca4bba8532fed5b2761b3f489f3d401ad4c9
ericaatann <ericatan@cpp.edu>
2023-12-02 16:43:26 -0800

    added encryption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
+from cryptography.fernet import Fernet
 import random
 from string import ascii_uppercase
 
@@ -9,13 +10,13 @@ app.config["SECRET_KEY"] = "secretKey"
 socketio = SocketIO(app)
 
 rooms = {}
+encryption_key = Fernet.generate_key()
+cipher_suite = Fernet(encryption_key)
 
 
 def generate_unique_code(length):
     while True:
-        code = ""
-        for _ in range(length):
-            code += random.choice(ascii_uppercase)
+        code = "".join(random.choice(ascii_uppercase) for _ in range(length))
         if code not in rooms:
             break
     return code
@@ -25,6 +26,14 @@ def is_name_available(room, name):
     if room in rooms and "used_names" in rooms[room]:
         return name not in rooms[room]["used_names"]
     return True
+
+
+def encrypt_message(message):
+    return cipher_suite.encrypt(message.encode())
+
+
+def decrypt_message(encrypted_message):
+    return cipher_suite.decrypt(encrypted_message).decode()
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -64,21 +73,29 @@ def room():
     room = session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
-    return render_template("room.html", code= room, messages=rooms[room]["messages"])
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
 
 @socketio.on("message")
 def message(data):
     room = session.get("room")
     if room not in rooms:
         return
+
+    original_message = data["data"]
+    encrypted_message = encrypt_message(original_message)
+
     content = {
         "name": session.get("name"),
-        "message": data["data"]
+        "message": encrypted_message,
+        "original_message": original_message,
     }
+
     send(content, to=room)
     rooms[room]["messages"].append(content)
-    print(f"{session.get('name')} said: {data['data']}")
-    
+
+    print(f"{session.get('name')} said (original): {original_message}")
+    print(f"{session.get('name')} said (encrypted): {encrypted_message}")
+
 @socketio.on("connect")
 def connect(auth):
     room = session.get("room")
@@ -89,9 +106,13 @@ def connect(auth):
         leave_room(room)
         return
     join_room(room)
-    send({"name": name, "message": "has entered the room"}, to=room)
-    rooms[room]["members"]+=1
+    
+    # Instead of using send, emit the "message" event
+    socketio.emit("message", {"name": name, "original_message": "has entered the room"}, room=room)
+
+    rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
+
 
 @socketio.on("disconnect")
 def disconnect():
@@ -103,8 +124,12 @@ def disconnect():
         rooms[room]["members"] -= 1
         if rooms[room]["members"] <= 0:
             del rooms[room]
-    send({"name": name, "message": "has left the room"}, to=room)
+
+    # Instead of using send, emit the "message" event
+    socketio.emit("message", {"name": name, "original_message": "has left the room"}, room=room)
+
     print(f"{name} has left the room {room}")
 
+    
 if __name__ == "__main__":
     socketio.run(app, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
